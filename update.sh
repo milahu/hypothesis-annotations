aeb89928363a0203093ef64270cc5cbe33c0c9f9
Milan Hauth <milahu@gmail.com>
2025-06-30 09:47:29 +0200

    add update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+
+users=(
+  milahu
+)
+
+for user in "${users[@]}"; do
+  ./hypothesis-annotations-scraper/hypothesis-annotations-scraper.sh $user
+  mv $user.*.json hypothesis-annotations.$user.json
+  git add hypothesis-annotations.$user.json
+done
+
+git commit -m up
