9bedc58e6cb887d0a793e2712568537fa2cbe0c8
Milan Hauth <milahu@gmail.com>
2025-07-03 10:42:26 +0200

    prettify json

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ users=(
 for user in "${users[@]}"; do
   ./hypothesis-annotations-scraper/hypothesis-annotations-scraper.sh $user
   mv $user.*.json hypothesis-annotations.$user.json
+  cat hypothesis-annotations.$user.json | jq | sponge hypothesis-annotations.$user.json
   git add hypothesis-annotations.$user.json
 done
 
